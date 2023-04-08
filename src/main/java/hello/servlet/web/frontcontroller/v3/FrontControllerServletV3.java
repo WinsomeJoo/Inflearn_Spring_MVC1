97ab9ebc8900c3a9a6f438ca0da68ed496fbfab0
WinsomeJoo <tmdwn0603@naver.com>
2023-04-08 18:59:04 +0900

    FrontControllerServletV3 논리 주석으로 정리

--- src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -41,14 +41,14 @@ public class FrontControllerServletV3 extends HttpServlet {
 
         //paraMap
 
-        Map<String, String> paramMap = createParamMap(request);
+        Map<String, String> paramMap = createParamMap(request); //username, age등등 파라미터 정보를 Map 다 넣어주는 부분
 
         ModelView mv = controller.process(paramMap);
-        String viewName = mv.getViewName();
+        String viewName = mv.getViewName(); // 컨트롤로에서 논리경로를 가져옴 모델정보도 같이 갖고오긴 함
 
-        MyView view = viewResolver(viewName);
+        MyView view = viewResolver(viewName); //논리경로를 물리경로로 바꿔줌
 
-        view.render(mv.getModel(),request,response);
+        view.render(mv.getModel(),request,response); //mv.getModel()을 넣는 이유는 model정보를 request에 담아 뷰에 뿌리려고
 
 
     }
