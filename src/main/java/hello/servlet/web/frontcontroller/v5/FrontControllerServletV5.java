8f0ea343de8b8703d629dda9be7629ab8b4311b8
WinsomeJoo <tmdwn0603@naver.com>
2023-04-11 17:06:42 +0900

    4-6 유연한 컨트롤러1 - v5

---
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,86 @@
+package hello.servlet.web.frontcontroller.v5;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
+
+
+@WebServlet(name="frontControllerServletV5",urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    Map<String, Object> handlerMappingMap = new HashMap<>();
+    List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+
+    public FrontControllerServletV5() {
+        initHandlerMappingMap();
+        initHandlerAdapters();
+    }
+
+
+    private void initHandlerMappingMap() {
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+    }
+
+    private void initHandlerAdapters() {
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+    }
+
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Object handler = getHandler(request);
+
+        if(handler==null){
+            response.setStatus(HttpServletResponse. SC_NOT_FOUND);
+            return;
+        }
+
+        MyHandlerAdapter adapter=getHandlerAdapter(handler);
+
+
+        ModelView mv = adapter.handle(request,response,handler);
+        String viewName = mv.getViewName(); // 컨트롤로에서 논리경로를 가져옴 모델정보도 같이 갖고오긴 함
+
+        MyView view = viewResolver(viewName); //논리경로를 물리경로로 바꿔줌
+
+        view.render(mv.getModel(),request,response); //mv.getModel()을 넣는 이유는 model정보를 request에 담아 뷰에 뿌리려고
+
+
+
+    }
+
+    private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        for (MyHandlerAdapter adapter : handlerAdapters) {
+            if (adapter.support(handler)) {
+                return  adapter;
+            }
+        }
+        throw new IllegalArgumentException("handler adapter를 찾을 수 없습니다 handller"+ handler);
+    }
+
+    private Object getHandler(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        return handlerMappingMap.get(requestURI);
+    }
+
+    private  MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+}
